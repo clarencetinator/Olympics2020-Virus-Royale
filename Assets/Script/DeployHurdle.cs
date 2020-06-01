6f8e2173d042a02af23650cc00a30a92ed493de3
clarencetinator <clarence.dimafelix07@gmail.com>
2020-06-01 16:34:00 +0800

    CovidRoyale Dev 2.0
    Added:
    Virus move when player hit hurdles.
    Game Over when virus gets close to player.
    Game Over Scene(still no design)
    Score text(no design)

---
+++ Assets/Script/DeployHurdle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeployHurdle : MonoBehaviour
+{
+
+    public GameObject hurdlePrefab;
+    public float respawnTime = 1.0f;
+    public Transform[] spawnPoint;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        StartCoroutine(HurdleWave());
+    }
+
+    private void SpawnHurdles()
+    {
+        GameObject a = Instantiate(hurdlePrefab, spawnPoint[Random.Range(0, spawnPoint.Length)].position, Quaternion.identity) as GameObject;
+        //a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y * -2);
+    }
+
+    IEnumerator HurdleWave()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            SpawnHurdles();
+        }
+        
+
+    }
+    
+}
